def96d8e2da07ccba015ea6ba17087f8a4d5d68c
walderlyjunior <155538836+WalderlyJR@users.noreply.github.com>
2026-02-10 10:18:33 -0300

    Refactor index.js for improved bot functionality

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { Boom } = require('@hapi/boom');
 const pino = require('pino');
 const OpenAI = require('openai');
 
-const SESSION_PATH = '/app/sessao_chikbijuwhatAp';
+const SESSION_PATH = '/app/sessao_chikbijuwhatsAp';
 let pairingRequested = false;
 
 if (!fs.existsSync(SESSION_PATH)) {
@@ -162,6 +162,7 @@ async function iniciarBot() {
 }
 
 iniciarBot();
+
 
 
 
