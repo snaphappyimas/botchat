41c643da2db0f81da9a802d441e3c20c6ea5491c
walderlyjunior <155538836+WalderlyJR@users.noreply.github.com>
2026-02-10 11:08:54 -0300

    Refactor bot initialization and system prompt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { Boom } = require('@hapi/boom');
 const pino = require('pino');
 const OpenAI = require('openai');
 
-const SESSION_PATH = '/app/sessao_chikbijuWhatsApp';
+const SESSION_PATH = '/app/sessao_chikbijuWhatsAppp';
 let pairingRequested = false;
 
 if (!fs.existsSync(SESSION_PATH)) {
@@ -162,6 +162,7 @@ async function iniciarBot() {
 }
 
 iniciarBot();
+
 
 
 
