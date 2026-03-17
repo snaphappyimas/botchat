98c7a381dac318c8198d48945a4652bb72d8b6d7
walderlyjunior <155538836+WalderlyJR@users.noreply.github.com>
2026-03-16 21:32:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 require('http').createServer((req, res) => res.end('Bot Online')).listen(PORT);
 
 // MUDANÇA AQUI: Usando a pasta /tmp para evitar erros de permissão do Railway
-const SESSION_PATH = '/tmp/sessao_limpei_nome_v1';
+const SESSION_PATH = '/tmp/sessao_limpei_nome_vv1';
 const UMA_HORA = 60 * 60 * 1000;
 const PALAVRA_PAUSA = "#pausar";
 const PALAVRA_VOLTAR = "#voltar";
