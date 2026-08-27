18e9c67b3991ba77ec808890bda5dfdfd031bce0
walderlyjunior <155538836+WalderlyJR@users.noreply.github.com>
2026-08-27 12:47:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const historico = {};
 const atendimentoHumano = {};
 
 // -----------------------------------------------------------------------------
-// PROMPT ATENDIMENTO
+// PROMPT DE ATENDIMENTO
 // -----------------------------------------------------------------------------
 
 const SYSTEM_PROMPT = `Você é a Andreia Costa, especialista e vendedora do Método Ímãs Lucrativos e distribuidora oficial das Máquinas Americanas de fotoímãs.
